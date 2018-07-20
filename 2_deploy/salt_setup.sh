534167af6f74bfb08cebed8be3f588eda3ff7b7b
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-07-20 16:40:33 +0200

    basic TCs - TC002

--- 2_deploy/salt_setup.sh
+++ 2_deploy/salt_setup.sh
@@ -19,7 +19,7 @@ set -x
 # configure salt master
 sudo rm /tmp/configure_salt_master.sh
 cat <<EOF > /tmp/configure_salt_master.sh 
-set -ex
+set -x
 SALT_MASTER_IP=\$(ip a s dev eth0|grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"|grep -v 255)
 zypper in -y deepsea
 sed -i "/#interface: 0.0.0.0/c\interface: \${SALT_MASTER_IP}" /etc/salt/master
@@ -27,6 +27,7 @@ sed -i "/#timeout: 5/c\timeout: 25" /etc/salt/master
 sed -i "/#master: salt/c\master: ${NAME_BASE}1" /etc/salt/minion
 systemctl enable salt-minion.service;systemctl start salt-minion.service;systemctl status salt-minion.service
 systemctl enable salt-master.service;systemctl start salt-master.service;systemctl status salt-master.service
+set +x
 EOF
 
 ssh root@${NAME_BASE}1 'bash -s' < /tmp/configure_salt_master.sh
@@ -34,7 +35,7 @@ ssh root@${NAME_BASE}1 'bash -s' < /tmp/configure_salt_master.sh
 # configure salt minions 
 sudo rm /tmp/configure_salt_minion.sh
 cat <<EOF > /tmp/configure_salt_minion.sh
-set -ex 
+set -x 
 MASTER=${NAME_BASE}1
 zypper in -y salt-minion
 sed -i "/#master: salt/c\master: \$MASTER" /etc/salt/minion
@@ -44,6 +45,7 @@ systemctl stop ntpd
 ntpdate -bs cz.pool.ntp.org
 systemctl start ntpd
 sntp -S -c \$MASTER || echo
+set +x
 EOF
 
 for (( i=2; i <= $VM_NUM; i++ ))
--- 3_tests/01_basic_TCs/TC002_rm_OSD_with_deepsea.sh
+++ 3_tests/01_basic_TCs/TC002_rm_OSD_with_deepsea.sh
@@ -3,7 +3,14 @@
 set -ex
 
 ceph osd tree
-
+disk=$(mount|grep ceph|awk -F '/' '{print $3}'|head -c 3)
+osd_id=$(mount|grep ceph|awk -F 'ceph-' '{print $2}'|awk -F 'type' '{print $1}'|head -n 1)
+salt-run disengage.safety
+salt-run remove.osd $osd_id
+# verification
+ceph osd tree
+systemctl status ceph-osd@${osd_id}.service|grep Active|grep "inactive (dead)" && echo "Service down: OK"
+mount|grep ceph|grep $disk || echo "Disk unmounted: OK"
 
 set +ex
 
