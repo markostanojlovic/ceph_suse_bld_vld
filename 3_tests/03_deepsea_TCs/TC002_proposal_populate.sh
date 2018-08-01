f1f1d47d6225d1e48546944da1e190d2876651e3
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-01 11:23:35 +0200

    DS proposal populate

--- 3_tests/03_deepsea_TCs/TC002_proposal_populate.sh
+++ 3_tests/03_deepsea_TCs/TC002_proposal_populate.sh
@@ -11,6 +11,11 @@ echo "Log path: " $LOG
 
 ssh root@$MASTER salt-run proposal.populate encryption=dmcrypt name=qatest >> $LOG 2>&1
 ssh root@$MASTER "cat \$(ls /srv/pillar/ceph/proposals/profile-qatest/stack/default/ceph/minions/*|tail -n 1 )" >> $LOG 2>&1
+# TODO make a better check, now, manuall check needs to be done 
+echo
+echo "DONT forget to manually check profiles!"
+echo "ssh root@$MASTER cat /srv/pillar/ceph/proposals/profile-qatest/stack/default/ceph/minions/*"
+echo
 
 echo "Result: OK"
 
