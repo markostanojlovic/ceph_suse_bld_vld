bda210856fcd0dae47bf934bb4263736621d9bf6
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-16 17:51:00 +0200

    NFS mount client test simplified

--- 3_tests/client/NFS_mount_tests.sh
+++ 3_tests/client/NFS_mount_tests.sh
@@ -3,10 +3,12 @@
 # USAGE:
 # ./NFS_client_test.sh NFS_IP
 
-set -e
+set -ex
 
-[[ -n $1 ]] && NFS_IP=$1 || (echo ERROR: Missing NFS IP; exit 1)
-timeout_limit=10   
+[[ -n $1 ]] && NFS_IP=$1
+
+TIMEOUT=10   
+
 MOUNTS=/tmp/mounts
 ##### mount options input file #####
 echo "\
@@ -16,36 +18,20 @@ mount -t nfs
 mount -t nfs -o rw,sync 
 mount.nfs4 " > $MOUNTS
 ####################################
-mount_target="${NFS_IP}:/ /mnt"
-openssl rand -base64 10000000 -out /tmp/random.txt
 
-function test_command_for_timeout {
-	command_to_test=$1
-	timeout $timeout_limit $command_to_test
-	timeout_rc=$?
-	if [[ $timeout_rc == 0 ]]
-		then
-		echo "INFO: command: [ $command_to_test ] finished OK" 
-	else
-		echo "ERROR: command: [ $command_to_test ] timed out after $timeout_limit seconds"; exit 1 
-    fi
-}
+openssl rand -base64 10000000 -out /tmp/random.txt # 13MB 
+mount|grep '/mnt ' && sudo umount /mnt -f 
+ping -q -c 3 $NFS_IP
 
-mount|grep mnt && umount /mnt -f 
-# test ping
-ping -q -c 3 $NFS_IP|grep " 0% packet loss" || ( echo "PING status: *** KO ***";exit 1 )
-echo "PING status: OK " 
-# TESTING
 while read mount_options
 do
-sleep 60
-test_command_for_timeout "$mount_options $mount_target"
-test_command_for_timeout "ls /mnt/cephfs"
-test_command_for_timeout "cp /tmp/random.txt /mnt/cephfs/nfs-ganesha_test_file_$(date +%H_%M_%S)"
-test_command_for_timeout "tail -n 1 /mnt/cephfs/nfs-ganesha_test_file_*"
-test_command_for_timeout 'umount /mnt'
+sudo timeout $TIMEOUT $mount_options ${NFS_IP}:/ /mnt
+sudo timeout $TIMEOUT ls /mnt/cephfs
+sudo timeout $TIMEOUT cp /tmp/random.txt /mnt/cephfs/nfs-ganesha_test_file_$(date +%H_%M_%S)
+sudo timeout $TIMEOUT tail -n 1 /mnt/cephfs/nfs-ganesha_test_file_*
+sudo timeout $TIMEOUT umount /mnt
 done < $MOUNTS
 
 echo 'Result: OK'
 
-set +e
+set +ex
