6c5330d4d909f3a6773dd6a35ae978ea43b27955
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-24 16:27:53 +0200

    verbose on ses4deploy

--- 2_deploy/ses4_deploy_deepsea.sh
+++ 2_deploy/ses4_deploy_deepsea.sh
@@ -17,7 +17,6 @@ scp cfg/policy.cfg root@${NAME_BASE}1:/tmp/
 # Deployment script 
 DEPL_SCRIPT=/tmp/salt_ses_deploy.sh
 cat <<EOF > $DEPL_SCRIPT
-set +x
 salt-run state.orch ceph.stage.0
 salt '*' cmd.run 'zypper up -y'
 salt-run state.orch ceph.stage.1
@@ -25,11 +24,10 @@ cp /tmp/policy.cfg /srv/pillar/ceph/proposals/policy.cfg
 salt-run state.orch ceph.stage.2
 salt-run state.orch ceph.stage.3
 salt-run state.orch ceph.stage.4
-set -x
 EOF
 
 # Deployment
-ssh root@$MASTER 'bash -s' < $DEPL_SCRIPT
+ssh root@$MASTER 'bash -sx' < $DEPL_SCRIPT
 [ $? -eq 0 ] && echo "Deployment OK" || exit 1
 
 echo "Result: OK"
