a37ea1d31f6af5cdf4ea86efbd6b452da8a0dfb4
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-27 12:00:24 +0200

    ses4 nfs deploy

--- 2_deploy/ses4_deploy_deepsea.sh
+++ 2_deploy/ses4_deploy_deepsea.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# openattic is deployed on master node
+# to be able to access, configure iptables on thunderx host:
+#  - delete all REJECT rules from FORWARD chain
+#  - add rule: iptables -t nat -I PREROUTING -d 10.161.32.49 -p tcp --dport 80 -j DNAT --to-destination ses4node1
+#  - verify with: iptables -t nat -L PREROUTING
 
 if [[ -z $1 ]]
 then
@@ -26,9 +31,38 @@ salt-run state.orch ceph.stage.3
 salt-run state.orch ceph.stage.4
 EOF
 
+# NFS-ganesha deployment 
+NFS_DEPL=/tmp/nfs-ganesha_deployment.sh
+cat <<EOF > $NFS_DEPL
+zypper in -y nfs-ganesha
+zypper in -y nfs-ganesha-ceph
+zypper in -y nfs-ganesha-rgw
+cat <<EOSF > /etc/ganesha/ganesha.conf
+EXPORT
+{
+  Export_Id = 1;
+  Path = "/";
+  Pseudo = "/";
+  Access_Type = RW;
+  Squash = No_Root_Squash;
+  FSAL {
+    Name = CEPH;
+  }
+}
+EOSF
+# RGW FSAL TODO 
+sudo systemctl enable rpcbind rpc-statd
+sudo systemctl start rpcbind rpc-statd
+sudo systemctl enable nfs-ganesha
+sudo systemctl start nfs-ganesha
+showmount -e
+EOF
+
 # Deployment
 ssh root@$MASTER 'bash -sx' < $DEPL_SCRIPT
 [ $? -eq 0 ] && echo "Deployment OK" || exit 1
+
+ssh root@${NAME_BASE}4 'bash -sx' < $NFS_DEPL
 
 echo "Result: OK"
 
