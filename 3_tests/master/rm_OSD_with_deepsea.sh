d2ce8cd1eb9d1762c81d61d9b9fc06f869ef7337
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-29 19:10:24 +0200

    remove.osd force=True

--- 3_tests/master/rm_OSD_with_deepsea.sh
+++ 3_tests/master/rm_OSD_with_deepsea.sh
@@ -7,7 +7,7 @@ ceph osd tree
 disk=$(_get_osd_disk_dev)
 osd_id=$(_get_osd_id)
 salt-run disengage.safety
-salt-run remove.osd $osd_id
+salt-run remove.osd $osd_id force=True
 # verification
 ceph osd tree
 systemctl status ceph-osd@${osd_id}.service|grep Active|grep "inactive (dead)" && echo "Service down: OK"
