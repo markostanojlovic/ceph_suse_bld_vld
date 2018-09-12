2c7e06894d6d53afc9cad94eedf49c0766b6633c
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-09-12 17:39:01 +0200

    add_OSD_with_deepsea modified

--- 3_tests/master/add_OSD_with_deepsea.sh
+++ 3_tests/master/add_OSD_with_deepsea.sh
@@ -4,6 +4,12 @@ set -ex
 
 ceph osd tree
 lsblk
+# add vdd on ses5node5 that is excluded in deployment 
+salt-run proposal.populate name=new
+orig=/srv/pillar/ceph/proposals/profile-default/stack/default/ceph/minions/ses5node5.qalab.yml
+cat /srv/pillar/ceph/proposals/profile-new/stack/default/ceph/minions/ses5node5.qalab.yml > $orig
+salt-run state.orch ceph.stage.2
+salt-run advise.osds
 salt-run state.orch ceph.stage.3
 # verification
 ceph osd tree
