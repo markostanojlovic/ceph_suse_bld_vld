d14dd256093a7d53c9dcf120ac717227af3483c6
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-09-12 17:16:19 +0200

    bv m9 thunderx

--- 2_deploy/deepsea_deploy_commands.sh
+++ 2_deploy/deepsea_deploy_commands.sh
@@ -23,6 +23,13 @@ salt-run state.orch ceph.stage.1
 echo "mon allow pool delete = true" >> /srv/salt/ceph/configuration/files/ceph.conf.d/global.conf
 echo "declare -x POL_CFG=/srv/pillar/ceph/proposals/policy.cfg" >> ~/.profile; .  ~/.profile
 cp /tmp/policy.cfg /srv/pillar/ceph/proposals/policy.cfg
+# --------------------------------------------------------------------------------
+# remove one OSD for the purpose of testing disk replacement with different drive 
+cd /srv/pillar/ceph/proposals/profile-default/stack/default/ceph/minions
+FILE=$(ls|tail -n 1) # ses5node5
+cat $FILE|head -n -2 > tmp.file; cat tmp.file > $FILE;rm tmp.file
+cd -
+# --------------------------------------------------------------------------------
 salt-run state.orch ceph.stage.2
 salt-run state.orch ceph.stage.3
 sed -i "/Transports/a Squash = No_Root_Squash;" /srv/salt/ceph/ganesha/files/ganesha.conf.j2
