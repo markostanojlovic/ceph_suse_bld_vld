286fea6e0d302282ca15a4fe83aabf93f6975e5d
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-10-01 16:15:32 +0200

    m10 x86

--- 3_tests/03_deepsea_TCs/TC005_migrate_filestore_bluestore.sh
+++ 3_tests/03_deepsea_TCs/TC005_migrate_filestore_bluestore.sh
@@ -9,8 +9,9 @@ echo $NAME_BASE
 LOG=$(setup_log_path $@)
 echo "Log path: " $LOG
 
-ssh root@$MASTER "salt-run proposal.populate format=filestore name=qatest" >> $LOG 2>&1
-ssh root@$MASTER "sed -i 's/profile-default/profile-qatest/g' /srv/pillar/ceph/proposals/policy.cfg" >> $LOG 2>&1
+# TODO check this, it's already done in deployment script
+#ssh root@$MASTER "salt-run proposal.populate format=filestore name=qatest" >> $LOG 2>&1
+#ssh root@$MASTER "sed -i 's/profile-default/profile-qatest/g' /srv/pillar/ceph/proposals/policy.cfg" >> $LOG 2>&1
 ssh root@$MASTER "salt-run state.orch ceph.migrate.policy" >> $LOG 2>&1
 # TODO verify that policy and profiles are populated correctly 
 ssh root@$MASTER "salt-run disengage.safety" >> $LOG 2>&1
