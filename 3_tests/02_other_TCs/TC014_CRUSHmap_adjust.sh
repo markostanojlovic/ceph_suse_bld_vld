95361924ecf4d2662e20eea2402706fcd2ff3a97
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-12-07 17:38:59 +0100

    crush: add rule

--- 3_tests/02_other_TCs/TC014_CRUSHmap_adjust.sh
+++ 3_tests/02_other_TCs/TC014_CRUSHmap_adjust.sh
@@ -10,6 +10,7 @@ LOG=$(setup_log_path $@)
 echo "Log path: " $LOG
 
 # TODO : find a way to assert or check if the new crushmap is as expected
+# TODO : create a new replicated rule and move all pools to a new rule
 
 ssh root@$MASTER 'bash -s' < 3_tests/master/CRUSH_split_to_2racks.sh $VM_NUM $NAME_BASE >> $LOG 2>&1
 
